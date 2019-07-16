f0792e5a27c28a64538ad2b22b0ffacc7275ea1e
Michał Karolczak <Michal.Karolczak1@ibm.com>
2019-07-16 15:48:09 +0000

    new hello there with name

--- test/api/controllers/hello_world.js
+++ test/api/controllers/hello_world.js
@@ -38,7 +38,7 @@ describe('controllers', function() {
           .expect(200)
           .end(function(err, res) {
             should.not.exist(err);
-
+			
             res.body.should.eql('Hello there, Scott!');
 
             done();
